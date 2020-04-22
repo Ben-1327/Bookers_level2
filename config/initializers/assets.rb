ed57ca9641a749f84b07c5784eb9d0c7e8d1cc58
Ben-1327 <ff.walker1327@gmail.com>
2020-04-22 11:27:31 +0900

    [Add] registrations_new

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( homes_list.scss homes_index.scss homes_about.scss)
+Rails.application.config.assets.precompile += %w( homes_list.scss homes_index.scss homes_about.scss registrations_new.scss )
